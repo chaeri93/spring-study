f637a9e6426c8deae7f9dab4ba8f12acd1c40367
Cherry <79985974+chaeri93@users.noreply.github.com>
2022-09-18 23:07:00 +0900

    feat: 스프링 빈 메타정보 확인

--- core/src/test/java/spring/core/xml/XmlAppContext.java
+++ core/src/test/java/spring/core/xml/XmlAppContext.java
@@ -3,7 +3,6 @@ package spring.core.xml;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
-import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
 
 import static org.assertj.core.api.Assertions.assertThat;
