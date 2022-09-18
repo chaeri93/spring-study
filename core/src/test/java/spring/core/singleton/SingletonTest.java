adb0a11c3edd191cbb015a8511f73922fb9946bf
Cherry <79985974+chaeri93@users.noreply.github.com>
2022-09-18 23:58:23 +0900

    feat: 무상태로 설계

--- core/src/test/java/spring/core/singleton/SingletonTest.java
+++ core/src/test/java/spring/core/singleton/SingletonTest.java
@@ -3,6 +3,8 @@ package spring.core.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.Appconfig;
 import spring.core.member.MemberService;
 
@@ -37,5 +39,20 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(Appconfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
---
+++ core/src/test/java/spring/core/singleton/StatefulService.java
@@ -0,0 +1,17 @@
+package spring.core.singleton;
+
+public class StatefulService {
+
+    private int price; //상태를 유지하는 필드
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + "price = " + price);
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
+
+// 공유 필드 조심 스프링 빈은 항상 무상태로 설계!!
