8c843f67c405caf3bc012ac3ae956fd5412dc925
chaeri93 <chaeri93@ewhain.net>
2024-01-30 23:08:53 +0900

    [feat] HTTP 요청 데이터

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request); // org.apache.catalina.connector.RequestFacade@7b7f60dc
+        System.out.println("response = " + response); // org.apache.catalina.connector.ResponseFacade@19f9268d
+
+        String username = request.getParameter("username"); // 쿼리파라미터 조회
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+
+}
