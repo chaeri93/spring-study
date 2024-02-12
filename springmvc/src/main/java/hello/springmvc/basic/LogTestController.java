fe548a2fff2948a3bf385356b6abe5aa15e2768b
chaeri93 <chaeri93@ewhain.net>
2024-02-13 01:49:36 +0900

    [feat] HTTP 요청

---
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,22 @@
+package hello.springmvc.basic;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Slf4j
+@RestController //반환 값으로 뷰를 찾는 것이 아니라, HTTP 메시지 바디에 바로 입력한다. 따라서 실행 결과로 ok 메세지를 받을 수 있다
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+        //로그를 사용하지 않아도 a+b 계산 로직이 먼저 실행됨, 이런 방식으로 사용하면 X log.debug("String concat log=" + name);
+        return "ok";
+    } }
